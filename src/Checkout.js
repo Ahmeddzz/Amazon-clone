e8be05825f1d5a4ce8749ede708e432f31fc38c9
Ahmeddzz <ahmedzahran95@hotmail.com>
2021-08-21 16:58:33 +0200

    Checkout refined

--- src/Checkout.js
+++ src/Checkout.js
@@ -12,29 +12,29 @@ function Checkout() {
             <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg"
                 alt="" />
-            
-            <div className="checkout__title">
-                <h2>Your Shopping Basket</h2>
-                <h3>Hello {user? user.email : 'Guest'}</h3>
+                <div className="checkout__title">
+                    <h2>Your Shopping Basket</h2>
+                    <h3>Hello {user? user.email : 'Guest'}</h3>
 
-                {basket.map(item => (
-                    <CheckoutProduct
-                    id = {item.id}
-                    title  ={item.title}
-                    image = {item.image}
-                    price = {item.price}
-                    rating = {item.rating}
-                    />
-                ))}
+                    {basket.map(item => (
+                        <CheckoutProduct
+                        id = {item.id}
+                        title  ={item.title}
+                        image = {item.image}
+                        price = {item.price}
+                        rating = {item.rating}
+                        />
+                    ))}
 
-            </div>
+                </div>
             </div>
 
             <div className="checkout__right">
             <Subtotal />
             </div>
+            
         </div>
     )
 }
 
-export default Checkout
+export default Checkout;
